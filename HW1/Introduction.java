87525f012e4c82fffa30d6f6be7fc803ef5d4cab
Vinh <24020364@vnu.edu.vn>
2025-09-21 17:37:40 +0700

    B

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Huu Vinh\t24020364\t2526I_INT2204_4\t24020364-NguyenHuuVinh\t24020364@vnu.edu.vn");
+        for (int i = 9; i > 1; i--) {
+            System.out.println( i + " bottles of beer on the wall, " + i  + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
